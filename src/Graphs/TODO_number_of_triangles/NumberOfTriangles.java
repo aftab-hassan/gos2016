9a1e4ad9acb5a3edccbb248f91427a5de742d3b6
aftab90 <aftabonquora@gmail.com>
2016-12-04 10:46:52 -0800

    TODO_number of tiangles in a graph

---
+++ src/Graphs/TODO_number_of_triangles/NumberOfTriangles.java
@@ -0,0 +1,16 @@
+package Graphs.TODO_number_of_triangles;
+
+
+/*
+ * Tushar Roy : https://github.com/mission-peace/interview/blob/master/src/com/interview/graph/NumberofTriangles.java
+ * Career Cup :  http://www.careercup.com/question?id=5988741646647296
+ * My SO qn   :http://math.stackexchange.com/questions/2009493/finding-number-of-distinct-walks-between-two-vertices-in-a-graph-using-matrix-mu
+ */
+public class NumberOfTriangles {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
